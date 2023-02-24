2621c05ca9436a2f5901722a6875180cc7c1857a
Lolineko30000 <317292181@pcpuma.acatlan.unam.mx>
2023-02-23 20:59:55 -0600

    pagina funcionando

--- crear_cuenta.php
+++ crear_cuenta.php
@@ -2,64 +2,166 @@
 //C:\xampp\mysql\bin
 include('Clases/DB.php');
 
-if(isset($_POST['create'])){
+if (isset($_POST['create'])) {
 
-        #Dedinicion de las variables que se introducen en 
-        #el formulario por medio del parametro name
-        $nombre = $_POST['name'];
-        $apellido = $_POST['surname'];
-        $nombre_usuario = $_POST['username'];
-        $contrasenia = $_POST['password'];
-        $correo = $_POST['correo'];
-        
+    #Dedinicion de las variables que se introducen en 
+    #el formulario por medio del parametro name
+    $nombre = $_POST['name'];
+    $apellido = $_POST['surname'];
+    $nombre_usuario = $_POST['username'];
+    $contrasenia = $_POST['password'];
+    $correo = $_POST['correo'];
 
-            #Comprobacion que el usuario ya exista
-        if(DB::query('SELECT nombre_usuario FROM usuario WHERE nombre_usuario = :nombre_usuario', array(':nombre_usuario' => $nombre_usuario ))){
-            header ("Location: index.html");
-        }
-        #Comprobacion del correo electronico
-        elseif(DB::query('SELECT correo FROM usuario WHERE correo = :correo', array(':correo' => $correo ))){
-            header ("Location: index.html");
-        }
-        #Tamanio de la contrasena y el nombre de usuario
-        elseif ((strlen($nombre_usuario) < 3 || strlen($nombre_usuario) > 40)  && !(preg_match('/[a-zA-Z0-9_]+/', $nombre_usuario))) {
-            header ("Location: index.html");
-        }elseif (strlen($contrasenia) < 6 || strlen($contrasenia) > 40) {
-            header("Location: index.html");
-        }
-        else{
 
-            #Encriptacion de la contasenioa pk alch estoy aburrido xd
-            $contrasenia = password_hash($contrasenia, PASSWORD_BCRYPT );
-            $pk_usuario = 1+DB::query('SELECT MAX(id_usuario) AS pkt FROM usuario', array())[0]['pkt'];
-            
+    #Comprobacion que el usuario ya exista
+    if (DB::query('SELECT nombre_usuario FROM usuario WHERE nombre_usuario = :nombre_usuario', array(':nombre_usuario' => $nombre_usuario))) {
+        header("Location: index.html");
+    }
+    #Comprobacion del correo electronico
+    elseif (DB::query('SELECT correo FROM usuario WHERE correo = :correo', array(':correo' => $correo))) {
+        header("Location: index.html");
+    }
+    #Tamanio de la contrasena y el nombre de usuario
+    elseif ((strlen($nombre_usuario) < 3 || strlen($nombre_usuario) > 40) && !(preg_match('/[a-zA-Z0-9_]+/', $nombre_usuario))) {
+        header("Location: index.html");
+    } elseif (strlen($contrasenia) < 6 || strlen($contrasenia) > 40) {
+        header("Location: index.html");
+    } else {
 
-            DB::query('INSERT INTO usuario  VALUES (:id_usuario, :nombre , :apellido , :correo , :nombre_usuario ,:contrasenia)',
-                                            array(':id_usuario'=>$pk_usuario,':nombre'=>$nombre , ':apellido'=>$apellido, ':correo'=>$correo,'nombre_usuario'=>$nombre_usuario,':contrasenia'=>$contrasenia));
-            
-            header ("Location: index.html");
-        }
+        #Encriptacion de la contasenioa pk alch estoy aburrido xd
+        $contrasenia = password_hash($contrasenia, PASSWORD_BCRYPT);
+        $pk_usuario = 1 + DB::query('SELECT MAX(id_usuario) AS pkt FROM usuario', array())[0]['pkt'];
+
+
+        DB::query(
+            'INSERT INTO usuario  VALUES (:id_usuario, :nombre , :apellido , :correo , :nombre_usuario ,:contrasenia)',
+            array(':id_usuario' => $pk_usuario, ':nombre' => $nombre, ':apellido' => $apellido, ':correo' => $correo, 'nombre_usuario' => $nombre_usuario, ':contrasenia' => $contrasenia)
+        );
+
+        header("Location: index.html");
+    }
 }
 
-                                                                                                                         
-        
+
 
 ?>
 
+<!DOCTYPE html>
+<html>
 
-<h1>Registro</h1>
-<form action= "crear_cuenta.php" method= "post" >
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Login Giorno</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' href='./css/style.css'>
+</head>
 
-<input type="text" name="name" value="" placeholder="Nombre(s)..."></p>
-<input type="text" name="surname" value="" placeholder="Apellidos..."></p>
+<body>
+    <header>
+        <h2 class="logo">Giorno</h2>
+        <nav class="navigation">
+            <a href="#">Viajes</a>
+            <a href="#">Transporte</a>
+            <a href="#">Hoteles</a>
+            <a href="#">Ofertas</a>
+            <button class="btnLogin-popup">Login</button>
 
-<input type="text" name="username" value="" placeholder="Nombre de usuario..."></p>
+        </nav>
+    </header>
 
-<input type="password" name="password" value="" placeholder="contrasenia..."></p>
+    <div class="wrapper">
+        <span class="icon-close">
+            <ion-icon name="close"></ion-icon>
+        </span>
 
-<input type="email" name="correo" value="" placeholder="Email..."></p>
+        <div class="form-box login">
+            <h2>Login</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" required>
+                    <label>Correo</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Contraseña </label>
+                </div>
+                <div class="remeber-forgot">
+                    <label><input type="checkbox">Recuerdame</label>
+                    <a href="#">Olvidaste la contraseña?</a>
+                </div>
+                <button type="submit" class="btn">Inicar sesión</button>
+                <div class="login-register">
+                    <p>No tienes una cuenta? <a href="#" class="register-link">Registrate</a></p>
+                </div>
+            </form>
+        </div>
 
-<input type="submit" name="create" value="Crear cuenta"></p>
 
-</form>
+        <div class="form-box register">
+            <h2>Registro</h2>
+            <form action="#">
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="person"></ion-icon>
+                    </span>
+                    <input type="text" required>
+                    <label>Usuario</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="mail"></ion-icon>
+                    </span>
+                    <input type="email" required>
+                    <label>Correo</label>
+                </div>
+                <div class="input-box">
+                    <span class="icon">
+                        <ion-icon name="lock-closed"></ion-icon>
+                    </span>
+                    <input type="password" required>
+                    <label>Contraseña </label>
+                </div>
+                <div class="remeber-forgot">
+                    <label><input type="checkbox">Estoy de acurdo
+                        con los terminos</label>
 
+                </div>
+                <button type="submit" class="btn">Registrar</button>
+                <div class="login-register">
+                    <p>Ya tengo una cuenta <a href="#" class="login-link">Iniciar sesión</a></p>
+                </div>
+            </form>
+        </div>
+    </div>
+    <!--
+    <h1>Registro</h1>
+    <form action="crear_cuenta.php" method="post">
+
+        <input type="text" name="name" value="" placeholder="Nombre(s)..."></p>
+        
+        <input type="text" name="surname" value="" placeholder="Apellidos..."></p>
+
+        <input type="text" name="username" value="" placeholder="Nombre de usuario..."></p>
+
+        <input type="password" name="password" value="" placeholder="contrasenia..."></p>
+
+        <input type="email" name="correo" value="" placeholder="Email..."></p>
+
+        <input type="submit" name="create" value="Crear cuenta"></p>
+
+    </form>
+    -->
+    <script src='/js/scripts.js'></script>
+    <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
+    <!--para buscar los iconos usados en la pagina o nuevos ir al url:https://ionic.io/ionicons -->
+</body>
+
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+const wrapper = document.querySelector('.wrapper')
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link')
+const btnPop= document.querySelector('.btnLogin-popup')
+const iconClose= document.querySelector('.icon-close')
+
+
+registerLink.addEventListener('click', () => {
+    wrapper.classList.add('active');//cambiar el nombre de la clase wrapper a wrapper active en html
+});
+loginLink.addEventListener('click', () => {
+    wrapper.classList.remove('active');//regresar el nombre orignal de la clase wrapper
+});
+
+btnPop.addEventListener('click', () => {
+    wrapper.classList.add('active-popup');//cambiar el nombre de la clase wrapper a wrapper active-popup en html
+});
+iconClose.addEventListener('click', () => {
+    wrapper.classList.remove('active-popup');//regresar el nombre orignal de la clase wrapper
+});

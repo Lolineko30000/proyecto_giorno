142bd71237a8d62bd2ccdc271c1fe7b2e469cd3b
Marielle <marielle280.machado@gmail.com>
2023-03-07 22:02:34 -0600

    Update crear_cuenta.php
    intentando crear la Página de viajes

--- crear_cuenta.php
+++ crear_cuenta.php
@@ -55,6 +55,8 @@ if (isset($_POST['create'])) {
     <title>Login Giorno</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' href='./css/style.css'>
+    <link rel="stylesheet" href="viajes.css">
+    <link rel="stylesheet" href="Viajes2.css">
 </head>
 
 <body>
@@ -162,6 +164,108 @@ if (isset($_POST['create'])) {
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
     <!--para buscar los iconos usados en la pagina o nuevos ir al url:https://ionic.io/ionicons -->
+
+    <!--Sección Viaje-->
+    <form action="#">
+        <div class="main-header">
+            <span class="icon-close">
+                <ion-icon name="close"></ion-icon>
+            </span>
+            <div class="content-header">
+                <h1>VIAJES</h1>
+                <p>La mejor forma de viajar</p>
+                <a href="#0"> reservar ahora</a>
+            </div>
+        </div>
+          
+          <section class="ftco-section ftco-no-pb">
+             <div class="container">
+                <div class="row">
+                 <div class="col-md-12">
+                    <div class="search-wrap-1 ftco-animate">
+                       <form action="#" class="search-property-1">
+                          <div class="row no-gutters">
+                             <div class="col-lg d-flex">
+                             <form action="#">
+                                <div class="form-group p-4 border-0">
+                                   <label for="#">Destino</label>
+                                   <div class="form-field">
+                                     <div class="icon"><span class="fa fa-search"></span></div>
+                                     
+                                     <select name="" id="" class="form-control">
+                                        <option value="">Acapulco</option>
+                                        <option value="">Cancun</option>
+                                        <option value="">Puerto Vallarta</option>
+                                    </select>
+                                 </div>
+                             </div>
+                                </form> 
+                         </div>
+                         <div class="col-lg d-flex">
+                         <form action="#">
+                            <div class="form-group p-4">
+                               <label for="#">Check-in</label>
+                               <div class="form-field">
+                                 <div class="icon"><span class="fa fa-calendar"></span></div>
+                                 <input type="date" class="form-control checkin_date" placeholder="Salida">
+                             </div>
+                         </div>
+                     </div>
+</form>
+                     <div class="col-lg d-flex">
+                     <form action="#">
+                        <div class="form-group p-4">
+                           <label for="#">Check-out </label>
+                           <div class="form-field">
+                             <div class="icon"><span class="fa fa-calendar"></span></div>
+                             <input type="date" class="form-control checkout_date" placeholder="Llegada">
+                         </div>
+                     </div>
+                 </div>
+</form>
+                 <div class="col-lg d-flex">
+                    <form action="#">
+                    <div class="form-group p-4">
+                       <label for="#">Rango de Precios</label>
+                       <div class="form-field">
+                         <div class="select-wrap">
+                          <div class="icon"><span class="fa fa-chevron-down"></span></div>
+                          <select name="" id="" class="form-control">
+                            <option value="">$5,000</option>
+                            <option value="">$10,000</option>
+                            <option value="">$50,000</option>
+                            <option value="">$100,000</option>
+                            <option value="">$200,000</option>
+                            <option value="">$300,000</option>
+                            <option value="">$400,000</option>
+                            <option value="">$500,000</option>
+                            <option value="">$600,000</option>
+                            <option value="">$700,000</option>
+                            <option value="">$800,000</option>
+                            <option value="">$900,000</option>
+                            <option value="">$1,000,000</option>
+                            <option value="">$2,000,000</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+          </div>
+          <div class="col-lg d-flex">
+            <div class="form-group d-flex w-100 border-0">
+               <div class="form-field w-100 align-items-center d-flex">
+                  <input type="submit" value="Search" class="align-self-stretch form-control btn btn-primary">
+              </div>
+          </div>
+          </div>
+          </div>
+          </form>
+          </div>
+          </div>
+          </div>
+          </div>
+          </section>
+      
+</form>
 </body>
 
 </html>
